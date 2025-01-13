4e91e1932f98b481e55e8d2c0f7ce9bf8f7c6123
Thiago Fabri <154623272+TFabri970sw@users.noreply.github.com>
2025-01-13 09:37:53 -0300

    Add files via upload

---
+++ Project.js-1/Atividades/Nome/nome.js
@@ -0,0 +1,2 @@
+let nome = prompt('Qual é seu nome?')
+alert (`Boas vindas ${nome}`)
---
+++ Project.js-1/Atividades/Pontuação/Placar.js
@@ -0,0 +1,6 @@
+pontuaçao = prompt('Digite qual é sua pontuação!');
+if (pontuaçao >= 100){
+    alert('Parabéns, você venceu!');
+}else{
+    alert('Tente novamente para ganhar.');
+}
